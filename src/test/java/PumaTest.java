30a681a63a7b43e30c80de26a0be0c0dbdede086
seadmasetic <75685855+seomasha@users.noreply.github.com>
2024-12-23 15:36:49 +0100

    Initial commit

---
+++ src/test/java/PumaTest.java
@@ -0,0 +1,43 @@
+import io.github.cdimascio.dotenv.Dotenv;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PumaTest {
+    private static WebDriver webDriver;
+    private static String baseUrl;
+
+    @BeforeAll
+    public static void setUp() {
+        Dotenv dotenv = Dotenv.load();
+        String webDriverPath = dotenv.get("WEBDRIVER_URL");
+        System.setProperty("webdriver.chrome.driver", webDriverPath);
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        webDriver = new ChromeDriver(options);
+        baseUrl = "https://eu.puma.com/de/en/home";
+    }
+
+    @Test
+    public void testTitle() throws InterruptedException {
+        webDriver.get(baseUrl);
+        String actualTitle = webDriver.getTitle();
+        System.out.println("Actual title: " + actualTitle);
+        assertEquals("PUMA GERMANY | Shoes, Apparel and Accessories",
+                actualTitle, "Title does not match");
+        Thread.sleep(1000);
+    }
+
+
+    @AfterAll
+    public static void tearDown() {
+        if (webDriver != null) {
+            webDriver.quit();
+        }
+    }
+}
